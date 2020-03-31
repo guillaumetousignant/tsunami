1b56006efbcbb02df7bf1f2798f112a9b4ae203d
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-03-31 19:52:03 -0400

    meshes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "another_path_tracer.h"
 #include "entities/MeshGeometryUnstructured_t.h"
+#include "shapes/MeshUnstructured_t.h"
 
 #include <string>
 
@@ -10,8 +11,11 @@ int main(int argc, char **argv){
     std::string mesh_file = argv[1];
     std::string data_file = argv[2];
 
-    MeshGeometryUnstructured_t water_mesh(mesh_file);
-    MeshGeometryUnstructured_t sand_mesh(mesh_file);
+    MeshGeometryUnstructured_t water_mesh_geometry(mesh_file);
+    MeshGeometryUnstructured_t sand_mesh_geometry(mesh_file);
+
+    MeshUnstructured_t water_mesh();
+    MeshUnstructured_t sand_mesh();
 
     return 0;
 }
