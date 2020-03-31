88dd9997c0937b3810348a221b0169f822e4e248
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-03-31 19:39:44 -0400

    mesh

--- include/shapes/MeshUnstructured_t.h
+++ include/shapes/MeshUnstructured_t.h
@@ -1,30 +1,35 @@
 #ifndef MESH_T_H
 #define MESH_T_H
 
-#include "entities/Ray_t.h"
 #include "entities/Vec3f.h"
-#include "shapes/MeshTop_t.h"
 
 namespace APTracer { namespace Entities {
     class Material_t;
     class TransformMatrix_t;
-    class MeshGeometry_t;
-    class MaterialMap_t;
+    class Shape_t;
 }}
+
+class MeshGeometryUnstructured_t;
 
 using APTracer::Entities::Material_t;
 using APTracer::Entities::TransformMatrix_t;
-using APTracer::Entities::MeshGeometry_t;
-using APTracer::Entities::MaterialMap_t;
-using APTracer::Shapes::MeshTop_t;
+using APTracer::Entities::Vec3f;
+using APTracer::Entities::Shape_t;
 
-class MeshUnstructured_t final : public MeshTop_t{
+class MeshUnstructured_t {
     public:
-        MeshUnstructured_t(Material_t *material, TransformMatrix_t *transform_matrix, MeshGeometry_t* geom);
-        MeshUnstructured_t(MaterialMap_t *materialmap, TransformMatrix_t *transform_matrix, MeshGeometry_t* geom);
+        MeshUnstructured_t(Material_t *material, TransformMatrix_t *transform_matrix, MeshGeometryUnstructured_t* geom);
         virtual ~MeshUnstructured_t() final;
 
+        Material_t *material_;
+        TransformMatrix_t *transformation_;
+        MeshGeometryUnstructured_t* geom_;
+        unsigned int n_tris_;
+        Shape_t** triangles_; // Maybe should be triangle**?
+
+        virtual void update();
+        virtual Vec3f mincoord() const;
+        virtual Vec3f maxcoord() const;
         virtual void createTriangles() final;
-        virtual void createTriangles(MaterialMap_t *materialmap) final;
 };
 #endif
--- src/shapes/MeshUnstructured_t.cpp
+++ src/shapes/MeshUnstructured_t.cpp
@@ -1,34 +1,51 @@
 #include "shapes/MeshUnstructured_t.h"
-#include "entities/MeshGeometry_t.h"
-#include "entities/MaterialMap_t.h"
-#include "shapes/TriangleMesh_t.h"
+#include "entities/MeshGeometryUnstructured_t.h"
+#include "shapes/TriangleUnstructured_t.h"
 #include <iostream>
 #include <limits>
 
-MeshUnstructured_t::MeshUnstructured_t(APTracer::Entities::Material_t *material, APTracer::Entities::TransformMatrix_t *transform_matrix, APTracer::Entities::MeshGeometry_t* geom) 
-    : MeshTop_t(material, transform_matrix, geom) {
+MeshUnstructured_t::MeshUnstructured_t(APTracer::Entities::Material_t *material, APTracer::Entities::TransformMatrix_t *transform_matrix, MeshGeometryUnstructured_t* geom) 
+    : material_(material), transformation_(transform_matrix), geom_(geom), n_tris_(geom->n_elements_) {
     
     createTriangles();
+}       
+
+MeshUnstructured_t::~MeshUnstructured_t(){
+    if (triangles_ != nullptr){
+        for (unsigned int i = 0; i < n_tris_; i++){
+            if (triangles_[i] != nullptr){
+                delete triangles_[i];
+            }
+        }
+        delete [] triangles_;
+    }
 }
-
-MeshUnstructured_t::MeshUnstructured_t(APTracer::Entities::MaterialMap_t *materialmap, APTracer::Entities::TransformMatrix_t *transform_matrix, APTracer::Entities::MeshGeometry_t* geom) 
-    : MeshTop_t(materialmap->getFirst(), transform_matrix, geom) {
-
-    createTriangles(materialmap);
-}        
-
-MeshUnstructured_t::~MeshUnstructured_t(){}
 
 void MeshUnstructured_t::createTriangles(){    
     triangles_ = new APTracer::Entities::Shape_t*[n_tris_];
     for (unsigned int i = 0; i < n_tris_; i++){
-        triangles_[i] = new APTracer::Shapes::TriangleMesh_t(material_, transformation_, geom_, i);
+        triangles_[i] = new TriangleUnstructured_t(material_, transformation_, geom_, i);
     }
 }
 
-void MeshUnstructured_t::createTriangles(APTracer::Entities::MaterialMap_t *materialmap){
-    triangles_ = new APTracer::Entities::Shape_t*[n_tris_];
+void MeshUnstructured_t::update(){
     for (unsigned int i = 0; i < n_tris_; i++){
-        triangles_[i] = new APTracer::Shapes::TriangleMesh_t(materialmap->getMaterial(geom_->mat_[i]), transformation_, geom_, i);
+        triangles_[i]->update();
     }
+}
+
+Vec3f MeshUnstructured_t::mincoord() const {
+    Vec3f coord = Vec3f(std::numeric_limits<double>::infinity());
+    for (unsigned int i = 0; i < n_tris_; i++){
+        coord.min(triangles_[i]->mincoord());
+    }    
+    return coord;
+}
+
+Vec3f MeshUnstructured_t::maxcoord() const {
+    Vec3f coord = Vec3f(-std::numeric_limits<double>::infinity());
+    for (unsigned int i = 0; i < n_tris_; i++){
+        coord.max(triangles_[i]->maxcoord());
+    }
+    return coord;
 }
