eafc9798cb3d6e3583554a9e0dd6f3ab20816247
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-04-01 08:47:15 -0400

    added scatterer

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 
 #include <string>
 
+using APTracer::Entities::Vec3f;
+
 int main(int argc, char **argv){
     if (argc < 3) {
         std::cerr << "Error: input arguments with format 'mesh_file data_file'. Exiting." << std::endl;
@@ -14,11 +16,15 @@ int main(int argc, char **argv){
     MeshGeometryUnstructured_t water_mesh_geometry(mesh_file);
     MeshGeometryUnstructured_t sand_mesh_geometry(mesh_file);
 
-    APTracer::Materials::ReflectiveRefractive_t water();
-    APTracer::Materials::Diffuse_t sand();
+    APTracer::Materials::Absorber_t water_scatterer(Vec3f(0.0, 0.0, 0.0), Vec3f(0.0, 0.0, 0.0), 1000, 1000);
+
+    APTracer::Materials::ReflectiveRefractive_t water(Vec3f(0.0, 0.0, 0.0), Vec3f(1.0, 1.0, 1.0), 1.4, 10, &water_scatterer);
+    APTracer::Materials::Diffuse_t sand(Vec3f(0.0, 0.0, 0.0), Vec3f(1.0, 1.0, 1.0), 1.0);
 
     MeshUnstructured_t water_mesh(&water, new TransformMatrix_t(), &water_mesh_geometry);
     MeshUnstructured_t sand_mesh(&sand, new TransformMatrix_t(), &sand_mesh_geometry);
+
+    
 
     return 0;
 }
