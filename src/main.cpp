5f1c7831b15ded745662bc3b60fa5c5a7b44f6b1
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-05-08 14:44:52 -0400

    set distance by hand

--- src/main.cpp
+++ src/main.cpp
@@ -116,7 +116,7 @@ int main(int argc, char **argv){
     std::list<Medium_t*> medium_list = {&air, &air};
     Vec3f min_sand_coord = sand_mesh.mincoord();
     APTracer::Cameras::Cam_t camera(&camera_transform, "images/output.png", Vec3f(0.0, 0.0, 1.0), fov, subpix, &imgbuffer, medium_list, &sky, 16, 1.0);
-    camera.transformation_->translate(Vec3f(0.0, min_sand_coord[1], 0.0));
+    camera.transformation_->translate(Vec3f(0.0, 2.2 * min_sand_coord[1], 0.0));
     camera.transformation_->rotateXAxis(-30.0 * M_PI/180);
     camera.transformation_->translate(Vec3f(0.0, 0.0, -2500.0));
     camera.update();
